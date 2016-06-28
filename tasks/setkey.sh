6f19a7a50bcef12966ce642014d6b5785d6dde9c
Samuel Lapointe <samuellapointe@gmail.com>
2016-06-28 10:59:43 -0400

    Save api_key to file

--- tasks/setkey.sh
+++ tasks/setkey.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-echo "Enter a Google Maps API Key for localhost, or press enter to skip: "
-read api_key
-
 # Get examples directory
 script_dir=$(dirname "$0")
 examples="$script_dir/../examples/*.html"
+key_file="$script_dir/../api_key.txt"
 
-# If length is too short, skip
-key_length=${#api_key}
+if [ ! -f $key_file ]; then
+    echo "Enter a Google Maps API Key for localhost, or press enter to skip: "
+    read api_key
 
-if [ $key_length -le 1 ] ; then
-    echo "Skipping key step (examples might not work)"
-    exit 0
+    # If length is too short, skip
+    key_length=${#api_key}
+
+    if [ $key_length -le 1 ] ; then
+        echo "Skipping key step (examples might not work)"
+        exit 0
+    fi
+
+    # Save key to file
+    echo $api_key > api_key.txt
+else
+    api_key=$(cat $key_file)
 fi
 
 original_url="\"https://maps.googleapis.com/maps/api/js?v=3\""
