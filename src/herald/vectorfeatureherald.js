2862017a7406b8f9f4e2d0d6b5c435e37638630b
geopamplona <alejo.sarfson@geoactio.com>
2017-03-08 08:31:01 +0100

    Update vectorfeatureherald.js

--- src/herald/vectorfeatureherald.js
+++ src/herald/vectorfeatureherald.js
@@ -170,7 +170,6 @@ olgm.herald.VectorFeature.prototype.unwatchFeature_ = function(feature) {
   if (index !== -1) {
     // remove from features (internal)
     this.features_.splice(index, 1);
-
     if(this.cache_.length>0){
         // deactivate feature herald
         this.cache_[index].herald.deactivate();
